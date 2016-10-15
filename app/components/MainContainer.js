e757ba0d09c1412f9fc284d18b26f1ea7ad99fc0
Chris Praktish <chris.praktish@gmail.com>
2016-10-15 19:47:23 -0400

    Add UI for results page and refactor to use private stateless functional component

---
+++ app/components/MainContainer.js
@@ -0,0 +1,12 @@
+React = require('react');
+styles = require('../styles');
+
+function MainContainer (props) {
+  return (
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      {props.children}
+    </div>
+  )
+};
+
+module.exports = MainContainer;
